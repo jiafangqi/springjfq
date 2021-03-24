7bc5af5f9969578e91e49227b4bce07d2133c68c
jiafangqi <277412772@qq.com>
2021-03-24 20:20:14 +0800

    init

---
+++ src/main/java/com/jfq/prringboot/PrringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jfq.prringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jfq/prringboot/PrringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfq.prringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
